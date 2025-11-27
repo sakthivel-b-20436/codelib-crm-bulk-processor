12fb3f637454c223a2f9d625d57302637d40da5e
sakthivel-b-20436 <sakthivel.b@zohocorp.com>
2025-11-27 11:54:54 +0530

    Added GitHub Actions workflow and updated functions code

--- functions/BulkDataProcessor/processor/RecordsProcessor.js
+++ functions/BulkDataProcessor/processor/RecordsProcessor.js
@@ -1,13 +1,21 @@
 /**
-   * Processes an array of Zoho CRM records.
-   * @param {Array<Object>} zcrmRecords - An array of records retrieved from Zoho CRM.
-   * Each object in the array is expected to have a `data` property.
-   * Example structure: `[{ data: { First_Name: '...', Last_Name: '...', Phone: '...' } }, ...]
-   * @returns {Array<Object>} - The array of Zoho CRM records after processing and modification.
-*/
+ * Processes an array of Zoho CRM records.
+ *
+ * @param {Array<{
+ *   module: string,
+ *   data: Record<string, any>
+ * }>} zcrmRecords - Array of Zoho CRM records returned by API.
+ *
+ * @returns {Array<{
+ *   module: string,
+ *   data: Record<string, any>
+ * }>} Processed Zoho CRM records.
+ */
 
 class RecordsProcessor {
   processRecords(zcrmRecords) {
+    zcrmRecords
+   
     return zcrmRecords;
   }
 }
--- functions/BulkJobScheduler/index.js
+++ functions/BulkJobScheduler/index.js
@@ -1,21 +1,26 @@
-const catalyst = require("zcatalyst-sdk-node");
+/**
+ * 
+ * @param {import("./types/job").JobRequest} jobRequest 
+ * @param {import("./types/job").Context} context 
+ */
 
+const catalyst = require("zcatalyst-sdk-node");
 module.exports = async (jobRequest, context) => {
   try {
     const app = catalyst.initialize(context);
     const module = jobRequest.getJobParam("MODULE");
-    const fields_to_be_processed = jobRequest.getJobParam("FIELDS_TO_BE_PROCESSED");
+    const fieldsToBeProcessed = jobRequest.getJobParam("FIELDS_TO_BE_PROCESSED");
 	if (!module) {
       throw new Error("MODULE cannot be empty");
     }
-    if (!fields_to_be_processed) {
+    if (!fieldsToBeProcessed) {
       throw new Error("FIELDS_TO_BE_PROCESSED cannot be empty");
     }
 
     const table = app.datastore().table("BulkRead");
     await table.insertRow({
       MODULE_NAME: module,
-      FIELDS_TO_BE_PROCESSED: fields_to_be_processed.replace(/\s/g, "") 
+      FIELDS_TO_BE_PROCESSED: fieldsToBeProcessed.replace(/\s/g, "") 
     });
     console.log("Inserted SucessFully");
   } catch (err) {
